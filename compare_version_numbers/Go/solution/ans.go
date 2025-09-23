0632fd5c0381306b01c0327a312433bd6c785b3f
ctrl-miztery <khang.ng.h.le@gmail.com>
2025-09-23 12:43:52 -0500

    update solution

--- compare_version_numbers/Go/solution/ans.go
+++ compare_version_numbers/Go/solution/ans.go
@@ -12,37 +12,25 @@ func CompareVersion(version1 string, version2 string) int {
 	v1Size := len(v1)
 	v2Size := len(v2)
 
-	if v1Size < v2Size {
-		diff := v2Size - v1Size
-		for diff > 0 {
-			v1 = append(v1, "0")
-			diff--
+	maxSize := max(v1Size, v2Size)
+
+	for i := range maxSize {
+		var ver1, ver2 int
+
+		if i < v1Size {
+			ver1, _ = strconv.Atoi(v1[i])
 		}
-	} else if v2Size < v1Size {
-		diff := v1Size - v2Size
-		for diff > 0 {
-			v2 = append(v2, "0")
-			diff--
+
+		if i < v2Size {
+			ver2, _ = strconv.Atoi(v2[i])
+		}
+
+		if ver1 > ver2 {
+			return 1
+		} else if ver2 > ver1 {
+			return -1
 		}
 	}
 
-	for i := 0; i < len(v1); i++ {
-		if ok, res := compareArr(v1[i], v2[i]); ok {
-			return res
-		}
-	}
 	return 0
-}
-
-func compareArr(v1, v2 string) (bool, int) {
-	num1, _ := strconv.Atoi(v1)
-	num2, _ := strconv.Atoi(v2)
-
-	if num1 == num2 {
-		return false, 0
-	} else if num1 > num2 {
-		return true, 1
-	} else {
-		return true, -1
-	}
 }
